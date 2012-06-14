5166376d67dd3dd2ec11e4fcd36f9df73195d609
Tom Clarkson <tom@orangeguava.com>
2012-06-15 01:35:12 +1000

    view improvements

--- src/view.js
+++ src/view.js
@@ -1,120 +1,125 @@
 Chondric.QuickView = function(container, options) {
-        var view = this;
-        var settings = {
-            change: function(val) {},
-            fields: {}
+    var view = this;
+    var settings = {
+        fields: {}
+    }
+
+    $.extend(settings, options);
+
+
+    view.onControlValueChanged = function(field, knownNewVal) {
+        var newVal = knownNewVal;
+        if (newVal === undefined) {
+            if (field.get) {
+                // custom getter
+                newVal = field.get(field.element)
+            } else {
+                newVal = field.element.val();
+            }
+        }
+        if (newVal != field.currentValue) {
+            field.currentValue = newVal;
+            if (field.change) {
+                field.change(field.currentValue);
+            }
         }
 
-        $.extend(settings, options);
+    }
+
+    this.initField = function(fieldname) {
+        var field = settings.fields[fieldname];
+        if (!field) {
+            console.log("field not found");
+            return;
+        }
+
+        if (field.init) field.init();
+
+        if (field.selector && field.element == undefined) {
+            field.element = $(field.selector, container);
+            console.log("init "+field.element.attr("id"));
+            field.element.bind("change keyup", function() {
+                view.onControlValueChanged(field);
+            });
+
+        }
+    }
+
+    // clone the fields so that the fields passed in the options can be reused
+    var f2 = settings.fields;
+    settings.fields = {};
+    for (var fn in f2) {
+        settings.fields[fn] = $.extend({}, f2[fn])
+        view.initField(fn);
+    }
+
+    this.prop = function(fieldname, value, shouldTriggerChange) {
+        var field = settings.fields[fieldname];
+        if (!field) {
+            console.log("field not found");
+            return;
+        }
+        // init field if not already set up
+        if (field.selector && field.element == undefined) {
+            view.initField(fieldname);
+        }
+        if (value === undefined) {
+            if (field.currentValue === undefined) {
+                if (field.get) {
+                    // custom getter
+                    console.log("custom get " + fieldname);
+
+                    return field.currentValue = field.get(field.element)
+                } else {
+                    console.log("standard get " + fieldname);
+
+                    return field.currentValue = field.element.val();
+                }
+            } else {
+                console.log("get from cache " + fieldname);
+                return field.currentValue;
+            }
+            // get value;
+        } else {
+            // set value
+            if (value == field.currentValue) {
+                console.log("unchanged " + fieldname);
+                return;
+            }
+            if (field.set) {
+                // custom setter
+                console.log("custom set " + fieldname);
+                field.currentValue = field.set(field.element, field.currentValue = value)
+            } else {
+                console.log("standard set " + fieldname);
+                field.element.val(field.currentValue = value);
+            }
+            console.log("changed " + fieldname);
+            if (shouldTriggerChange && field.change) {
+                console.log("calling change " + fieldname);
+                field.change(field.currentValue);
+            }
+
+        }
+    };
 
 
-        this.initField = function(fieldname) {
+    this.invalidate = function(fieldname) {
+        if (fieldname == undefined) {
+            for (var fn in settings.fields) {
+                delete settings.fields[fn].currentValue;
+            }
+        } else {
             var field = settings.fields[fieldname];
             if (!field) {
                 console.log("field not found");
                 return;
             }
-
-            if (field.selector && field.element == undefined) {
-                field.element = $(field.selector, container);
-
-                field.element.bind("change keyup", function() {
-                    var newVal = undefined;
-                    if (field.get) {
-                        // custom getter
-                        console.log("custom get on change " + fieldname);
-                        newVal = field.get(field.element)
-                    } else {
-                        console.log("standard get on change " + fieldname);
-                        newVal = field.element.val();
-                    }
-
-                    if (newVal != field.currentValue) {
-                        field.currentValue = newVal;
-                        console.log("changed " + fieldname);
-                        if (field.change) {
-                            console.log("calling change " + fieldname);
-                            field.change(field.currentValue);
-                        }
-                    }
-
-
-                });
-
-            }
+            delete field.currentValue;
         }
 
-        for (var fn in settings.fields) {
-            view.initField(fn);
-        }
 
-        this.prop = function(fieldname, value, shouldTriggerChange) {
-            var field = settings.fields[fieldname];
-            if (!field) {
-                console.log("field not found");
-                return;
-            }
-            // init field if not already set up
-            if (field.selector && field.element == undefined) {
-                view.initField(fieldname);
-            }
-            if (value === undefined) {
-                if (field.currentValue === undefined) {
-                    if (field.get) {
-                        // custom getter
-                        console.log("custom get " + fieldname);
-
-                        return field.currentValue = field.get(field.element)
-                    } else {
-                        console.log("standard get " + fieldname);
-
-                        return field.currentValue = field.element.val();
-                    }
-                } else {
-                    console.log("get from cache " + fieldname);
-                    return field.currentValue;
-                }
-                // get value;
-            } else {
-                // set value
-                if (value == field.currentValue) {
-                    console.log("unchanged " + fieldname);
-                    return;
-                }
-                if (field.set) {
-                    // custom setter
-                    console.log("custom set " + fieldname);
-                    field.currentValue = field.set(field.element, field.currentValue = value)
-                } else {
-                    console.log("standard set " + fieldname);
-                    field.element.val(field.currentValue = value);
-                }
-                console.log("changed " + fieldname);
-                if (shouldTriggerChange && field.change) {
-                    console.log("calling change " + fieldname);
-                    field.change(field.currentValue);
-                }
-
-            }
-        };
-
-
-        this.invalidate = function(fieldname) {
-            if (fieldname == undefined) {
-                for (var fn in settings.fields) {
-                    delete settings.fields[fn].currentValue;
-                }
-            } else {
-                var field = settings.fields[fieldname];
-                if (!field) {
-                    console.log("field not found");
-                    return;
-                }
-                delete field.currentValue;
-            }
-
-
-
-        };
 
     };
+
+};
