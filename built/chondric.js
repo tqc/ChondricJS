d6e33e09afb4724a5fde2f9813da74d9d34c8cfc
Tom Clarkson <tom@orangeguava.com>
2014-04-07 15:54:48 +1000

    add error handling functionality to loading overlay

--- built/chondric.js
+++ built/chondric.js
@@ -1277,10 +1277,14 @@ Chondric.directive('ngTap', function() {
         }
     };
 })
+// add a loading overlay if the scope has dataLoadStatus.waitingForData set.
+// if dataLoadStatus.error is set, it will be displayed as an error message.
+// if dataLoadStatus.retry is a function, a button wil be displayed
+// if dataLoadStatus.cancel is set, a button will be displayed.
 Chondric.directive('loadingOverlay', function($compile) {
     return {
         replace: true,
-        template: '<div class="loadingoverlay" ng-show="waitingForData"><div class="progress large"><div></div></div><div>Loading</div></div>'
+        template: '<div class="loadingoverlay" ng-show="dataLoadStatus.waitingForData"><div ng-show="!dataLoadStatus.error" class="progress large"><div></div></div><div ng-show="!dataLoadStatus.error">Loading</div><div class="error" ng-show="dataLoadStatus.error">{{dataLoadStatus.error}}</div><div><button ng-show="dataLoadStatus.retry && dataLoadStatus.error" ng-tap="dataLoadStatus.retry()">Retry</button><button ng-show="dataLoadStatus.cancel" ng-tap="dataLoadStatus.cancel()">Cancel</button></div></div>'
     }
 });
 Chondric.directive("cjsPopover", function() {
