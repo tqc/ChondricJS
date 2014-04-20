93bacb5f27766d6eb7427ac1f7ad8cc452dbf2ac
Tom Clarkson <tom@orangeguava.com>
2014-04-20 14:37:54 +1000

    transition updates

---
+++ src/transitions/crossfade.js
@@ -0,0 +1,70 @@
+Chondric.allTransitions.crossfade = {
+    transitionIn: {
+        start: function(element) {
+            // put new element in front with 0 opacity
+            element.css({
+                "display": "block",
+                "opacity": 0,
+                "z-index": 9000,
+            })
+        },
+        cancel: function(element, prevProgress) {
+            // move element to left with transition
+            var time = (prevProgress) * 300;
+            $(element).css({
+                "-webkit-transition": "opacity " + time + "ms ease-in-out",
+                "opacity": 0
+            })
+
+            return time;
+        },
+        complete: function(element, prevProgress) {
+            // set transform to 0 with transition
+            var time = (1 - prevProgress) * 300;
+            $(element).css({
+                "-webkit-transition": "opacity " + time + "ms ease-in-out",
+                "opacity": 1
+            })
+            return time;
+        },
+        progress: function(element, progress) {
+            element.css({
+                "display": "block",
+                "opacity": progress,
+                "z-index": 9000,
+            })
+        }
+    },
+    transitionOut: {
+        start: function(element) {
+            // just ensure the oldpage remains visible
+            element.css({
+                "display": "block"
+            })
+        },
+        cancel: function(element, prevProgress) {
+            var time = (prevProgress) * 300;
+            return time;
+        },
+        complete: function(element, prevProgress) {
+            // move element to right with transition
+            var time = (1 - prevProgress) * 300;
+            return time;
+        },
+        progress: function(element, progress) {
+            // set element position without transition
+            element.css({
+                "display": "block"
+            })
+        }
+    },
+    reset: function(element) {
+        // remove transition, transform and display settings from relevant subelements
+        element.css({
+            "display": "",
+            "opacity": "",
+            "z-index": "",
+            "-webkit-transition": "",
+        })
+    }
+};
