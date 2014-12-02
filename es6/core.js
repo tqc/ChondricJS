b7dd55b144ccf2cdf5389a70ea9a8c672b1993e0
Tom Clarkson <tom@orangeguava.com>
2014-12-03 09:35:06 +1100

    lasttap fix

--- es6/core.js
+++ es6/core.js
@@ -341,7 +341,7 @@ export class App {
 
             $scope.changePage = function(a, b, c) {
                 // because angular expressions can pass in lastTap but not lastTap.element in angular 1.3
-                if (c.element) c = c.element;
+                if (c && c.element) c = c.element;
                 app.changePage(a, b, c);
             };
 
