b5ab7bd80c3cec55ed0e1131e5e0b186711b7afb
Tom Clarkson <tom@orangeguava.com>
2015-08-26 14:29:10 +1000

    allow build error reports as html

--- buildtools/reporterror.js
+++ buildtools/reporterror.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
     vp.addClass("viewport-error").removeClass("viewport-preload");
 
     var title = "[TITLE]";
-    if (title) $("<h1>").appendTo(vp).text(title);
+    if (title) $("<h1>").appendTo(vp).html(title);
 
     var msg = "[MESSAGE]";
-    if (msg) $("<div>").addClass("message").appendTo(vp).text(msg);
+    if (msg) $("<div>").addClass("message").appendTo(vp).html(msg);
     var src = "[SOURCE]";
-    if (src) $("<div>").addClass("source").appendTo(vp).text(src);
+    if (src) $("<div>").addClass("source").appendTo(vp).html(src);
     var detail = "[DETAIL]";
-    if (detail) $("<div>").addClass("detail").appendTo(vp).text(detail);
+    if (detail) $("<div>").addClass("detail").appendTo(vp).html(detail);
 
 });
